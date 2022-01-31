ec3d3a4be7306b217a2b6e52f23d19259a7ee433
kleonh92 <kleon@serempre.com>
2022-01-31 17:40:37 -0500

    Aplicando splide slider al home de directorio

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,7 +1,37 @@
 
 
-// $(document).ready(function(){
-//     $('.aliados--carousel').slick({
+var splide = new Splide( '.splide', {
+    type   : 'loop',
+    perPage: 4,
+    arrows: false,
+    pagination: false, 
+    focus: 'center',
+    autoplay: true,
+    speed: 3000,
+    width: '70%',
+    breakpoints: {
+        1440 : { 
+            perPage: 4,
+            focus: 'center'
+        },
+        1024 : {
+            perPage: 3,
+            focus: 'center',
+            width: '60%'
+        },
+        768 : {
+            perPage: 2,
+            focus: 'center',
+            width: '65%',
+        },
+        425: {
+            perPage: 1,
+            focus: 'center'
+        }
+        
 
-//     });
-//  });
+    }
+    
+  } );
+  
+  splide.mount();
