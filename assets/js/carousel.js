b9357ad921065f23dc6967c85b685840728bf0b4
kleonh92 <kleon@serempre.com>
2022-02-02 12:27:07 -0500

    Cambios en pagina de agencia

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -35,3 +35,40 @@ var splide = new Splide( '.splide', {
   } );
   
   splide.mount();
+
+
+  var splide2 = new Splide( '.splide2', {
+    type   : 'loop',
+    perPage: 4,
+    arrows: false,
+    pagination: false, 
+    focus: 'center',
+    autoplay: true,
+    speed: 3000,
+    width: '70%',
+    breakpoints: {
+        1440 : { 
+            perPage: 4,
+            focus: 'center'
+        },
+        1024 : {
+            perPage: 3,
+            focus: 'center',
+            width: '60%'
+        },
+        768 : {
+            perPage: 2,
+            focus: 'center',
+            width: '65%',
+        },
+        425: {
+            perPage: 1,
+            focus: 'center'
+        }
+        
+
+    }
+    
+  } );
+  
+  splide2.mount();
