0352fccbfdf8e83f35aabf9bd02d9278f94adc95
kleonh92 <kleon@serempre.com>
2022-02-02 15:47:27 -0500

    Finalizado carousel en pantalla agencia

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -36,39 +36,3 @@ var splide = new Splide( '.splide', {
   
   splide.mount();
 
-
-  var splide2 = new Splide( '.splide-agencia', {
-    type   : 'loop',
-    perPage: 4,
-    arrows: false,
-    pagination: false, 
-    focus: 'center',
-    autoplay: true,
-    speed: 3000,
-    width: '70%',
-    breakpoints: {
-        1440 : { 
-            perPage: 4,
-            focus: 'center'
-        },
-        1024 : {
-            perPage: 3,
-            focus: 'center',
-            width: '60%'
-        },
-        768 : {
-            perPage: 2,
-            focus: 'center',
-            width: '65%',
-        },
-        425: {
-            perPage: 1,
-            focus: 'center'
-        }
-        
-
-    }
-    
-  } );
-  
-  splide2.mount();
